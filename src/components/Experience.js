abf070286889fe69d356824161ec41860c332f96
rashedrahat <imrashedrahat@gmail.com>
2023-10-25 01:50:21 +0600

    Updated 2023-2024

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,120 +1,138 @@
 import React, { useRef } from "react";
-import {motion, useScroll } from "framer-motion"
+import { motion, useScroll } from "framer-motion";
 import LiIcon from "./LiIcon";
 
-const Details = ({ position, company, companyLink, time, address, work, type }) => {
+const Details = ({
+                     position,
+                     company,
+                     companyLink,
+                     time,
+                     address,
+                     work,
+                     type,
+                 }) => {
     const ref = useRef(null);
-  return (
-    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]">
-
-      <LiIcon reference={ref}/>
-      <motion.div
-      initial={{y:50}}
-      whileInView={{y:0}}
-      transition={{duration:0.5, type:"spring"}}
-      >
-        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
-          {position}&nbsp;
-          <a
-            href={companyLink}
-            target="_blank"
-            className="text-primary dark:text-primaryDark capitalize"
-          >
-            @{company}
-          </a>
-        </h3>
-        <span className="font-medium text-dark/75 dark:text-light/75 xs:text-sm">
+    return (
+        <li
+            ref={ref}
+            className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%]"
+        >
+            <LiIcon reference={ref} />
+            <motion.div
+                initial={{ y: 50 }}
+                whileInView={{ y: 0 }}
+                transition={{ duration: 0.5, type: "spring" }}
+            >
+                <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+                    {position}&nbsp;
+                    <a
+                        href={companyLink}
+                        target="_blank"
+                        className="text-primary dark:text-primaryDark capitalize"
+                    >
+                        @{company}
+                    </a>
+                </h3>
+                <span className="font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {time} | {type} {address && `| ${address}`}
         </span>
-        <p className="font-medium w-full md:text-sm text-justify">{work}</p>
-      </motion.div>
-    </li>
-  );
+                <p className="font-medium w-full md:text-sm text-justify">{work}</p>
+            </motion.div>
+        </li>
+    );
 };
 
 const Experience = () => {
     const ref = useRef(null);
-    const {scrollYProgress} = useScroll(
-        {
-            target: ref,
-            offset: ["start end", "center start"]
-        }
-    )
-  return (
-    <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
-        Experience
-      </h2>
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "center start"],
+    });
+    return (
+        <div className="my-64">
+            <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+                Experience
+            </h2>
 
-      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
-
-<motion.div
-style={{scaleY: scrollYProgress}}
-  className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top
+            <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
+                <motion.div
+                    style={{ scaleY: scrollYProgress }}
+                    className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top
   md:w-[2px] md:left-[30px] xs:left-[20px] dark:bg-primaryDark dark:shadow-3xl
-  "  />
+  "
+                />
 
-        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
-        <Details
-              position="SDE II ReactJS Engineer"
-              company="Shikho"
-              companyLink="https://shikho.com"
-              time="Dec 2021 - Present"
-              type="Full-time"
-              address="Banani, Dhaka"
-              work="Highly accomplished professional with a track record of saving $1M annually through third-party tool integration, improving in-house product functionality by 15-25%, staying up-to-date with new release versions, and guiding junior developers in design, development, deployment, and maintenance."
-            />
+                <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+                    <Details
+                        position="Sr. Software Engineer, Front-end (ReactJS)"
+                        company="Shikho"
+                        companyLink="https://shikho.com"
+                        time="Aug 2023 - Present"
+                        type="Full-time"
+                        address="Banani, Dhaka"
+                        work='Guiding junior developers in design, development, deployment, and maintenance.'
+                    />
 
-            <Details
-                position="Software Engineer"
-                company="Valkyrie"
-                companyLink="https://valkyrie-it.com"
-                time="Oct 2021 - Mar 2022"
-                type="Part-time (Remote)"
-                work="Designed and developed MVPs or minimum viable products that offer the core benefits of the company's clients to its target users."
-            />
+                    <Details
+                        position="Software Engineer, Front-end (ReactJS)"
+                        company="Shikho"
+                        companyLink="https://shikho.com"
+                        time="Dec 2021 - Jul 2022"
+                        type="Full-time"
+                        address="Banani, Dhaka"
+                        work="Highly accomplished professional with a track record of saving $1M annually through third-party tool integration, improved in-house product functionality by 15-25%, stayed up-to-date with new release versions."
+                    />
 
-            <Details
-                position="Software Engineer (React.js)"
-                company="Misfit"
-                companyLink="https://misfit.tech"
-                time="Mar 2021 - Nov 2021"
-                type="Full-time"
-                address="Banani, Dhaka"
-                work="Implemented React expertise to improve component lifecycle procedures, achieving a 24% faster turnaround, 100% deadline adherence, and implemented a responsive, mobile-first approach that increased mobile traffic by 20%."
-            />
+                    <Details
+                        position="Software Engineer"
+                        company="Valkyrie"
+                        companyLink="https://valkyrie-it.com"
+                        time="Oct 2021 - Mar 2022"
+                        type="Part-time (Remote)"
+                        work="Designed and developed MVPs or minimum viable products that offer the core benefits of the company's clients to its target users."
+                    />
 
-            <Details
-                position="Full Stack Developer"
-                company="Domatic"
-                companyLink="https://domatictechnologies.com"
-                time="Mar 2020 - Jun 2021"
-                type="Part-time (Remote)"
-                work="Utilized React principles to design customer-facing responsive UIs, implemented Node.js for back-end services, and demonstrated strong troubleshooting and debugging skills in interface software and application codes."
-            />
+                    <Details
+                        position="Software Engineer, Front-end (ReactJS)"
+                        company="Misfit"
+                        companyLink="https://misfit.tech"
+                        time="Mar 2021 - Nov 2021"
+                        type="Full-time"
+                        address="Banani, Dhaka"
+                        work="Implemented React expertise to improve component lifecycle procedures, achieving a 24% faster turnaround, 100% deadline adherence, and implemented a responsive, mobile-first approach that increased mobile traffic by 20%."
+                    />
 
-            <Details
-                position="Laravel Developer"
-                company="DoorSoft"
-                companyLink="https://doorsoft.co"
-                time="Apr 2020 - Apr 2021"
-                type="Part-time (Remote)"
-                work="Designed and implemented Laravel-based web applications, implemented server-side logic for front-end inputs, and adeptly identified and fixed code bugs."
-            />
+                    <Details
+                        position="Full Stack Developer"
+                        company="Domatic"
+                        companyLink="https://domatictechnologies.com"
+                        time="Mar 2020 - Jun 2021"
+                        type="Part-time (Remote)"
+                        work="Utilized React principles to design customer-facing responsive UIs, implemented Node.js for back-end services, and demonstrated strong troubleshooting and debugging skills in interface software and application codes."
+                    />
 
-            <Details
-              position="Junior Software Engineer"
-              company="Workspace"
-              companyLink="https://www.workspaceit.com"
-              time="Jun 2019 - Mar 2021"
-              type="Full-time"
-              address="Nikunja 2, Dhaka"
-              work="Accomplished training projects utilizing Node.js and React to develop industry-standard web applications, demonstrated expertise in software testing, installation, configuration, and troubleshooting, and showcased proficiency in developing a wide range of SQL queries in MySQL and MongoDB."
-            />
-        </ul>
-      </div>
-    </div>
-  );
+                    <Details
+                        position="Laravel Developer"
+                        company="DoorSoft"
+                        companyLink="https://doorsoft.co"
+                        time="Apr 2020 - Apr 2021"
+                        type="Part-time (Remote)"
+                        work="Designed and implemented Laravel-based web applications, implemented server-side logic for front-end inputs, and adeptly identified and fixed code bugs."
+                    />
+
+                    <Details
+                        position="Junior Software Engineer"
+                        company="Workspace"
+                        companyLink="https://www.workspaceit.com"
+                        time="Jun 2019 - Mar 2021"
+                        type="Full-time"
+                        address="Nikunja 2, Dhaka"
+                        work="Accomplished training projects utilizing Node.js and React to develop industry-standard web applications, demonstrated expertise in software testing, installation, configuration, and troubleshooting, and showcased proficiency in developing a wide range of SQL queries in MySQL and MongoDB."
+                    />
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Experience;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -18,215 +18,211 @@ const FramerImage = motion(Image);
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article
-      className="w-full flex items-center justify-between relative  rounded-br-2xl
+      <article
+          className="w-full flex items-center justify-between relative  rounded-br-2xl
         rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 dark:bg-dark dark:border-light
         lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
-    >
-      <div
-        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
+      >
+        <div
+            className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light
     rounded-br-3xl xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]
     "
-      />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
-      >
-        <FramerImage
-          src={img}
-          alt={title}
-          className="w-full h-auto"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
-          priority
-          sizes="(max-width: 768px) 100vw,
+        />
+        <Link
+            href={link}
+            target="_blank"
+            className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full"
+        >
+          <FramerImage
+              src={img}
+              alt={title}
+              className="w-full h-auto"
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.2 }}
+              priority
+              sizes="(max-width: 768px) 100vw,
               (max-width: 1200px) 50vw,
               50vw"
-        />
-      </Link>
+          />
+        </Link>
 
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
         <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">
           {type}
         </span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light lg:text-3xl xs:text-2xl">
-            {title}
-          </h2>
-        </Link>
-        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm text-justify">
-          {summary}
-        </p>
-        <div className="mt-2 flex items-center">
-          {
-            github && (
-                  <Link href={github} target="_blank" className="w-10 mr-4">
-                    {" "}
-                    <GithubIcon />
-                  </Link>
-              )
-          }
           <Link
-            href={link}
-            target="_blank"
-            className="rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
+              href={link}
+              target="_blank"
+              className="hover:underline underline-offset-2"
+          >
+            <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light lg:text-3xl xs:text-2xl">
+              {title}
+            </h2>
+          </Link>
+          <p className="my-2 font-medium text-dark dark:text-light sm:text-sm text-justify">
+            {summary}
+          </p>
+          <div className="mt-2 flex items-center">
+            {github && (
+                <Link href={github} target="_blank" className="w-10 mr-4">
+                  {" "}
+                  <GithubIcon />
+                </Link>
+            )}
+            <Link
+                href={link}
+                target="_blank"
+                className="rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold
             dark:bg-light dark:text-dark
             sm:px-4 sm:text-base
             "
-          >
-            Visit Project
-          </Link>
+            >
+              Visit Project
+            </Link>
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
   );
 };
 
 const Project = ({ title, type, img, link, github, summary }) => {
   return (
-    <article
-      className="w-full flex flex-col items-center justify-center rounded-2xl
+      <article
+          className="w-full flex flex-col items-center justify-center rounded-2xl
     border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4
     "
-    >
-      <div
-        className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
+      >
+        <div
+            className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark
     rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%] xs:rounded-[1.5rem]
     "
-      />
-      <Link
-        href={link}
-        target="_blank"
-        className="w-full cursor-pointer overflow-hidden rounded-lg"
-      >
-        <FramerImage
-          src={img}
-          alt={title}
-          className="w-full h-auto"
-          whileHover={{ scale: 1.05 }}
-          transition={{ duration: 0.2 }}
         />
-      </Link>
+        <Link
+            href={link}
+            target="_blank"
+            className="w-full cursor-pointer overflow-hidden rounded-lg"
+        >
+          <FramerImage
+              src={img}
+              alt={title}
+              className="w-full h-auto"
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.2 }}
+          />
+        </Link>
 
-      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <div className="w-full flex flex-col items-start justify-between mt-4">
         <span className="text-primary font-medium text-xl dark:text-primaryDark lg:text-lg md:text-base">
           {type}
         </span>
-        <Link
-          href={link}
-          target="_blank"
-          className="hover:underline underline-offset-2"
-        >
-          <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
-            {title}
-          </h2>
-        </Link>
-        <p className="my-2 font-medium text-dark dark:text-light sm:text-sm text-justify">
-          {summary}
-        </p>
-
-        <div className="w-full flex items-center justify-between">
           <Link
-            href={link}
-            target="_blank"
-            className="text-lg font-semibold underline md:text-base"
+              href={link}
+              target="_blank"
+              className="hover:underline underline-offset-2"
           >
-            Visit
+            <h2 className="my-2 w-full text-left text-3xl font-bold lg:text-2xl">
+              {title}
+            </h2>
           </Link>
-          {
-            github && (
-                  <Link href={github} target="_blank" className="w-8 md:w-6">
-                    <GithubIcon />
-                  </Link>
-              )
-          }
+          <p className="my-2 font-medium text-dark dark:text-light sm:text-sm text-justify">
+            {summary}
+          </p>
+
+          <div className="w-full flex items-center justify-between">
+            <Link
+                href={link}
+                target="_blank"
+                className="text-lg font-semibold underline md:text-base"
+            >
+              Visit
+            </Link>
+            {github && (
+                <Link href={github} target="_blank" className="w-8 md:w-6">
+                  <GithubIcon />
+                </Link>
+            )}
+          </div>
         </div>
-      </div>
-    </article>
+      </article>
   );
 };
 
 const projects = () => {
   return (
-    <>
-      <Head>
-        <title>Rashed Rahat | Projects</title>
-      </Head>
-      <TransitionEffect />
-      <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
-        <Layout className="pt-16">
-          <AnimatedText
-            text="Imagination Trumps Knowledge!"
-            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
-          />
+      <>
+        <Head>
+          <title>Rashed Rahat | Projects</title>
+        </Head>
+        <TransitionEffect />
+        <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
+          <Layout className="pt-16">
+            <AnimatedText
+                text="Imagination Trumps Knowledge!"
+                className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+            />
 
-          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
-            <div className="col-span-12">
-              <FeaturedProject
-                type="Featured Project"
-                title="The New Health Club"
-                summary="A feature-rich The New Health Club website using React.js, Tailwind CSS and Context API. Re-introducing psychedelics into society. A global psychedelic community and spaces for transformation."
-                img={project1}
-                link="https://www.thenewhealthclub.de"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                type="E-commerce Website"
-                title="Shopoth"
-                summary="Trusted online shopping experience in Bangladesh from your local outlets anytime at fair price. Developed using Next.js, Tailwind CSS and Redux."
-                img={project2}
-                link="https://shopoth.com"
-              />
-            </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                type="E-learning Website"
-                title="Bohubrihi"
-                img={project3}
-                summary="Prepare yourself for a job or freelancing. Developed using Next.js, SASS, Ant Design and Context API."
-                link="https://bohubrihi.com"
-              />
-            </div>
+            <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
+              <div className="col-span-12">
+                <FeaturedProject
+                    type="Featured Project"
+                    title="The New Health Club"
+                    summary="A feature-rich The New Health Club website using React.js, Tailwind CSS and Context API. Re-introducing psychedelics into society. A global psychedelic community and spaces for transformation."
+                    img={project1}
+                    link="https://www.thenewhealthclub.de"
+                />
+              </div>
+              <div className="col-span-6 sm:col-span-12">
+                <Project
+                    type="E-commerce Website"
+                    title="Shopoth"
+                    summary="Trusted online shopping experience in Bangladesh from your local outlets anytime at fair price. Developed using Next.js, Tailwind CSS and Redux."
+                    img={project2}
+                    link="https://shopoth.com"
+                />
+              </div>
+              <div className="col-span-6 sm:col-span-12">
+                <Project
+                    type="E-learning Website"
+                    title="Bohubrihi"
+                    img={project3}
+                    summary="Prepare yourself for a job or freelancing. Developed using Next.js, SASS, Ant Design and Context API."
+                    link="https://bohubrihi.com"
+                />
+              </div>
 
-            <div className="col-span-12">
-              <FeaturedProject
-                type="Portfolio Website"
-                title="Valkyrie IT"
-                summary="An enterprise portfolio developed using Next.js and Tailwind CSS. It has unique design and it is mobile responsive."
-                img={project4}
-                link="https://www.valkyrie-it.com"
-              />
-            </div>
+              <div className="col-span-12">
+                <FeaturedProject
+                    type="Portfolio Website"
+                    title="Valkyrie IT"
+                    summary="An enterprise portfolio developed using Next.js and Tailwind CSS. It has unique design and it is mobile responsive."
+                    img={project4}
+                    link="https://www.valkyrie-it.com"
+                />
+              </div>
 
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                type="E-learning Website"
-                img={project5}
-                title="Shikho"
-                link="https://shikho.com"
-                summary="A feature-rich website Shikho using Next.js, Tailwind CSS and Styled-components. Shikho on a mission to revolutionise the way Bangladesh learns today and democratise access to a high quality education."
-              />
+              <div className="col-span-6 sm:col-span-12">
+                <Project
+                    type="E-learning Website"
+                    img={project5}
+                    title="Shikho"
+                    link="https://shikho.com"
+                    summary="A feature-rich website Shikho using Next.js, Tailwind CSS and Styled-components. Shikho on a mission to revolutionise the way Bangladesh learns today and democratise access to a high quality education."
+                />
+              </div>
+              <div className="col-span-6 sm:col-span-12">
+                <Project
+                    type="Social Website"
+                    img={project6}
+                    title="Facebook Instagram"
+                    summary="Clone of Facebook and Instagram using React.js and Tailwind CSS."
+                    link="https://tier5-reactjs-task.vercel.app"
+                    github="https://github.com/rashedrahat/tier5-reactjs-task"
+                />
+              </div>
             </div>
-            <div className="col-span-6 sm:col-span-12">
-              <Project
-                type="Social Website"
-                img={project6}
-                title="Facebook Instagram"
-                summary="Clone of Facebook and Instagram using React.js and Tailwind CSS."
-                link="https://tier5-reactjs-task.vercel.app"
-                github="https://github.com/rashedrahat/tier5-reactjs-task"
-              />
-            </div>
-          </div>
-        </Layout>
-      </main>
-    </>
+          </Layout>
+        </main>
+      </>
   );
 };
 
